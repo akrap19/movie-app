bf16f15faa5e62d809c35be77ca9165e20aadf0c
Matija Akrap <matija.akrap@gmail.com>
2020-08-19 18:25:42 +0200

    Updates

---
+++ precache-manifest.dfb83df5880ed4d5fab79b97844af752.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddcf5fbf6b2331852fe56aa0c05efcae",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "d768f60344d7976a7d92",
+    "url": "/movie-app/static/css/2.04ff9426.chunk.css"
+  },
+  {
+    "revision": "2922486282e2f8adc708",
+    "url": "/movie-app/static/css/main.494786e2.chunk.css"
+  },
+  {
+    "revision": "d768f60344d7976a7d92",
+    "url": "/movie-app/static/js/2.792fb8f8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie-app/static/js/2.792fb8f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2922486282e2f8adc708",
+    "url": "/movie-app/static/js/main.253058b0.chunk.js"
+  },
+  {
+    "revision": "7e869a2352de7a9f3c65",
+    "url": "/movie-app/static/js/runtime-main.85ad0f9f.js"
+  },
+  {
+    "revision": "2c4cea3184362ecdf0bc5eff642773e4",
+    "url": "/movie-app/static/media/mov2.2c4cea31.png"
+  },
+  {
+    "revision": "50ff51a0993f82ef03f37064ca39df9a",
+    "url": "/movie-app/static/media/undraw_Tree_swing_646s 1.50ff51a0.svg"
+  },
+  {
+    "revision": "e4ce87c138ab887e3c21f38cb4f9b356",
+    "url": "/movie-app/static/media/undraw_empty_street_sfxm 1.e4ce87c1.png"
+  },
+  {
+    "revision": "35a82d13c0a5d36a6f60e028e7a92cc8",
+    "url": "/movie-app/static/media/undraw_videographer_nnc7 1.35a82d13.svg"
+  }
+]);
